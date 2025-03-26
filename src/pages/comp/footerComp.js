94727adeaf89a8f01f5bd00ad854166da6299d49
n00210357 <n00210357@iadt.ie>
2025-03-26 15:58:09 +0000

    UI assempling 1

--- src/pages/comp/footerComp.js
+++ src/pages/comp/footerComp.js
@@ -8,65 +8,110 @@ export default function Bottom()
     return(
     <UserContextProvider>
         <footer className="container row align-items-center text-center mx-1 my-5 position-absolute bottom start-50 translate-middle">
-            <div className="col-2 align-items-center text-center flex-fill">
-                <button className="align-items-center text-center w-100 ms-1">
+            <div className="col-2 align-items-center text-center flex-fill butHov">
+                <button className="align-items-center text-center w-100 ms-1 rounded-1 border border-4 border-dark" data-toggle="tooltip" title="Home page">
                     <a href="../home">
-                        <h3>
-                            Home
-                        </h3>
+                        <div className='fw-bolder d-flex flex-row justify-content-center py-3'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-house-door-fill me-3" viewBox="0 0 16 16">
+                                <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5"/>
+                            </svg>
+
+                            <p className='my-0 d-none d-md-block'>
+                                Home
+                            </p>
+                        </div>
                     </a>
                 </button>
-            </div>
+            </div>    
 
-            <div className="col-2 align-items-center text-center flex-fill">
-                <button className="align-items-center text-center w-100 ms-1">
+            <div className="col-2 align-items-center text-center flex-fill butHov">
+                <button className="align-items-center text-center w-100 ms-1 rounded-1 border border-4 border-dark" data-toggle="tooltip" title="Create a puzzle">
                     <a href="../create">
-                        <h3>
-                            Create
-                        </h3>
+                        <div className='fw-bolder d-flex flex-row justify-content-center py-3'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-screwdriver me-3" viewBox="0 0 16 16">
+                                <path d="M0 .995.995 0l3.064 2.19a1 1 0 0 1 .417.809v.07c0 .264.105.517.291.704l5.677 5.676.909-.303a1 1 0 0 1 1.018.24l3.338 3.339a.995.995 0 0 1 0 1.406L14.13 15.71a.995.995 0 0 1-1.406 0l-3.337-3.34a1 1 0 0 1-.24-1.018l.302-.909-5.676-5.677a1 1 0 0 0-.704-.291H3a1 1 0 0 1-.81-.417zm11.293 9.595a.497.497 0 1 0-.703.703l2.984 2.984a.497.497 0 0 0 .703-.703z"/>
+                            </svg>
+
+                            <p className='my-0 d-none d-md-block'>
+                                Create
+                            </p>
+                        </div>
                     </a>
                 </button>
-            </div>
+            </div>   
 
-            <div className="col-2 align-items-center text-center flex-fill">
-                <button className="align-items-center text-center w-100 ms-1">
+            <div className="col-2 align-items-center text-center flex-fill butHov">
+                <button className="align-items-center text-center w-100 ms-1 rounded-1 border border-4 border-dark" data-toggle="tooltip" title="Search through puzzles">
                     <a href="../search">
-                        <h3>
-                            Search
-                        </h3>
+                        <div className='fw-bolder d-flex flex-row justify-content-center py-3'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-search me-3" viewBox="0 0 16 16">
+                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                            </svg>
+
+                            <p className='my-0 d-none d-md-block'>
+                                Search
+                            </p>
+                        </div>
                     </a>
                 </button>
-            </div>
+            </div>   
 
-            <div className="col-2 align-items-center text-center flex-fill">
-                <button className="align-items-center text-center w-100 ms-1">
+            <div className="col-2 align-items-center text-center flex-fill butHov">
+                <button className="align-items-center text-center w-100 ms-1 rounded-1 border border-4 border-dark" data-toggle="tooltip" title="Look for users">
                     <a href="../users">
-                        <h3>
-                            Users
-                        </h3>
+                        <div className='fw-bolder d-flex flex-row justify-content-center py-3'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-people-fill me-3" viewBox="0 0 16 16">
+                                <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>
+                            </svg>
+
+                            <p className='my-0 d-none d-md-block'>
+                                Users
+                            </p>
+                        </div>
                     </a>
                 </button>
-            </div>
+            </div>   
 
-            <div className="col-2 align-items-center text-center flex-fill">
-                <button className="align-items-center text-center w-100 ms-1">
+            <div className="col-2 align-items-center text-center flex-fill butHov">
+                <button className="align-items-center text-center w-100 ms-1 rounded-1 border border-4 border-dark" data-toggle="tooltip" title="Report a bug">
                     <a href="../bug">
-                        <h3>
-                            Bug report
-                        </h3>
-                    </a>
-                </button>
-            </div>
+                        <div className='fw-bolder d-flex flex-row justify-content-center py-3'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-bug-fill me-2" viewBox="0 0 16 16">
+                                <path d="M4.978.855a.5.5 0 1 0-.956.29l.41 1.352A5 5 0 0 0 3 6h10a5 5 0 0 0-1.432-3.503l.41-1.352a.5.5 0 1 0-.956-.29l-.291.956A5 5 0 0 0 8 1a5 5 0 0 0-2.731.811l-.29-.956z"/>
+                                <path d="M13 6v1H8.5v8.975A5 5 0 0 0 13 11h.5a.5.5 0 0 1 .5.5v.5a.5.5 0 1 0 1 0v-.5a1.5 1.5 0 0 0-1.5-1.5H13V9h1.5a.5.5 0 0 0 0-1H13V7h.5A1.5 1.5 0 0 0 15 5.5V5a.5.5 0 0 0-1 0v.5a.5.5 0 0 1-.5.5zm-5.5 9.975V7H3V6h-.5a.5.5 0 0 1-.5-.5V5a.5.5 0 0 0-1 0v.5A1.5 1.5 0 0 0 2.5 7H3v1H1.5a.5.5 0 0 0 0 1H3v1h-.5A1.5 1.5 0 0 0 1 11.5v.5a.5.5 0 1 0 1 0v-.5a.5.5 0 0 1 .5-.5H3a5 5 0 0 0 4.5 4.975"/>
+                            </svg>
 
-            <div className="col-2 align-items-center text-center flex-fill">
-                <button className="align-items-center text-center w-100 ms-1">
-                    <a href="../account">
-                        <h3>
-                            Account
-                        </h3>
+                            <div className='d-flex flex-row my-0'>                                
+                                <p className='my-0 d-none d-md-block pe-1'>
+                                    report
+                                </p>
+                                
+
+                                <p className='my-0 d-none d-xl-block ps-1'>
+                                    bug
+                                </p>
+                            </div>
+                        </div>
                     </a>
                 </button>
-            </div>
+            </div>   
+
+            <div className="col-2 align-items-center text-center flex-fill butHov">
+                <button className="align-items-center text-center w-100 ms-1 rounded-1 border border-4 border-dark" data-toggle="tooltip" title="Your account">
+                    <a href="../account">
+                        <div className='fw-bolder d-flex flex-row justify-content-center py-3'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-person-circle me-3" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                            </svg>
+
+                            <p className='my-0 d-none d-md-block'>
+                                Account
+                            </p>
+                        </div>
+                    </a>
+                </button>
+            </div>  
         </footer>
     </UserContextProvider>
     )
--- src/pages/comp/headerComp.js
+++ src/pages/comp/headerComp.js
@@ -21,46 +21,73 @@ export default function Top()
             <div className="col-3">
                 <ul className="nav justify-content-start align-items-center text-center">
                     <li className="nav-item px-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-star-fill min-logo" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-star-fill me-3" viewBox="0 0 16 16">
                             <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
                         </svg>  
                     </li>
 
                     <li className="nav-item px-2">
-                        <h4 className="fw-bold align-items-center text-center">
-                            Greetings {email}
-                        </h4>  
+                        <h4 className='align-items-center text-center my-3'>Greetings {email}</h4>
                     </li>
                 </ul>
             </div>
 
-            <div className="col-6">
-                <button id="clickMe" className="mx-3" value="your puzzles" type="button">
+            <div className="col-3 align-items-center text-center flex-fill butHov">
+                <button className="align-items-center text-center w-100 ms-1 rounded-1 border border-4 border-dark" data-toggle="tooltip" title="A page that only shows your puzzles">
                     <a href="/yourPuzzles">
-                        <h4>
-                            Your puzzles
-                        </h4>
+                        <div className='fw-bolder d-flex flex-row justify-content-center py-3'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-puzzle-fill me-3" viewBox="0 0 16 16">
+                                <path d="M3.112 3.645A1.5 1.5 0 0 1 4.605 2H7a.5.5 0 0 1 .5.5v.382c0 .696-.497 1.182-.872 1.469a.5.5 0 0 0-.115.118l-.012.025L6.5 4.5v.003l.003.01q.005.015.036.053a.9.9 0 0 0 .27.194C7.09 4.9 7.51 5 8 5c.492 0 .912-.1 1.19-.24a.9.9 0 0 0 .271-.194.2.2 0 0 0 .036-.054l.003-.01v-.008l-.012-.025a.5.5 0 0 0-.115-.118c-.375-.287-.872-.773-.872-1.469V2.5A.5.5 0 0 1 9 2h2.395a1.5 1.5 0 0 1 1.493 1.645L12.645 6.5h.237c.195 0 .42-.147.675-.48.21-.274.528-.52.943-.52.568 0 .947.447 1.154.862C15.877 6.807 16 7.387 16 8s-.123 1.193-.346 1.638c-.207.415-.586.862-1.154.862-.415 0-.733-.246-.943-.52-.255-.333-.48-.48-.675-.48h-.237l.243 2.855A1.5 1.5 0 0 1 11.395 14H9a.5.5 0 0 1-.5-.5v-.382c0-.696.497-1.182.872-1.469a.5.5 0 0 0 .115-.118l.012-.025.001-.006v-.003l-.003-.01a.2.2 0 0 0-.036-.053.9.9 0 0 0-.27-.194C8.91 11.1 8.49 11 8 11s-.912.1-1.19.24a.9.9 0 0 0-.271.194.2.2 0 0 0-.036.054l-.003.01v.002l.001.006.012.025c.016.027.05.068.115.118.375.287.872.773.872 1.469v.382a.5.5 0 0 1-.5.5H4.605a1.5 1.5 0 0 1-1.493-1.645L3.356 9.5h-.238c-.195 0-.42.147-.675.48-.21.274-.528.52-.943.52-.568 0-.947-.447-1.154-.862C.123 9.193 0 8.613 0 8s.123-1.193.346-1.638C.553 5.947.932 5.5 1.5 5.5c.415 0 .733.246.943.52.255.333.48.48.675.48h.238z"/>
+                            </svg>
+
+                            <p className='my-0 d-none d-md-block'>
+                                Your puzzles
+                            </p>
+                        </div>
                     </a>
                 </button>
+            </div> 
 
-                <button id="clickMe" className="mx-3" value="your puzzles" type="button">
+            <div className="col-3 align-items-center text-center flex-fill butHov">
+                <button className="align-items-center text-center w-100 ms-1 rounded-1 border border-4 border-dark" data-toggle="tooltip" title="The most recent survey">
                     <a href="https://forms.office.com/Pages/ResponsePage.aspx?id=e5V92hEVQkqy9Xj4R_jIes5I7qOcfq1Ni9f-_MqUtaRUMlk4OFJEMFFOV1ZURE5QTExOV0YwODVFOS4u">
-                        <h4>
-                            User Testing survey
-                        </h4>
-                    </a>
-                </button>
-            </div>
+                        <div className='fw-bolder d-flex flex-row justify-content-center py-3'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-newspaper me-3" viewBox="0 0 16 16">
+                            <path d="M0 2.5A1.5 1.5 0 0 1 1.5 1h11A1.5 1.5 0 0 1 14 2.5v10.528c0 .3-.05.654-.238.972h.738a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 1 1 0v9a1.5 1.5 0 0 1-1.5 1.5H1.497A1.497 1.497 0 0 1 0 13.5zM12 14c.37 0 .654-.211.853-.441.092-.106.147-.279.147-.531V2.5a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0-.5.5v11c0 .278.223.5.497.5z"/>
+                            <path d="M2 3h10v2H2zm0 3h4v3H2zm0 4h4v1H2zm0 2h4v1H2zm5-6h2v1H7zm3 0h2v1h-2zM7 8h2v1H7zm3 0h2v1h-2zm-3 2h2v1H7zm3 0h2v1h-2zm-3 2h2v1H7zm3 0h2v1h-2z"/>
+                        </svg>
 
-            <div className="col-3">
-                <button id="clickMe" className="mx-3 my-2" value="LOGOUT" type="button" onClick={handlePress}>
-                    <a href="/">
-                        <h3>
-                            Logout
-                        </h3>
+                        <div className='d-flex flex-row my-0'>                                
+                                <p className='my-0 d-none d-xl-block pe-1'>
+                                    User testing
+                                </p>
+                                
+
+                                <p className='my-0 d-none d-md-block ps-1'>
+                                    Survey
+                                </p>
+                            </div>
+                        </div>
                     </a>
                 </button>
-            </div>
+            </div>         
+
+            <div className="col-3 align-items-center text-center flex-fill butHov">
+                <button className="align-items-center text-center w-100 ms-1 rounded-1 border border-4 border-dark" data-toggle="tooltip" title="Logout and return to start page" onClick={handlePress}>
+                    <a href="/">
+                        <div className='fw-bolder d-flex flex-row justify-content-center py-3'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-box-arrow-l me-3" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z"/>
+                                <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z"/>
+                            </svg>
+
+                            <p className='my-0 d-none d-md-block'>
+                                Logout
+                            </p>
+                        </div>
+                    </a>
+                </button>
+            </div>  
         </nav>
     </UserContextProvider>
     )
